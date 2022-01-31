ae2d5442d8501bfb19f30459f534949f9a02c66b
Tanay Bhardwaj <tanaybhardwaj24@gmail.com>
2022-01-31 23:14:30 +0530

    v1 done. add docs

--- main.go
+++ main.go
@@ -18,28 +18,73 @@ func check(e error) {
 }
 
 func encodePassword(website string, userInput string) {
-	err := godotenv.Load(".env")
-	check(err)
 	API_KEY := os.Getenv("apiKey")
 	API_EMAIL := os.Getenv("apiMail")
 	ACCOUNT_ID := os.Getenv("accountID")
 	KV_NAMESPACE := os.Getenv("kvNamespace")
 	ctx := context.Background()
-	api, err := cloudflare.New(API_KEY, API_EMAIL, cloudflare.UsingAccount(ACCOUNT_ID))
+	client, err := cloudflare.New(API_KEY, API_EMAIL, cloudflare.UsingAccount(ACCOUNT_ID))
 	check(err)
 	key := website
 	payload := []byte(base64.StdEncoding.EncodeToString([]byte(userInput)))
-	resp, err := api.WriteWorkersKV(ctx, KV_NAMESPACE, key, payload)
+	resp, err := client.WriteWorkersKV(ctx, KV_NAMESPACE, key, payload)
+	check(err)
+	fmt.Println(resp)
+}
+
+func getPassword(website string) {
+	API_KEY := os.Getenv("apiKey")
+	API_EMAIL := os.Getenv("apiMail")
+	ACCOUNT_ID := os.Getenv("accountID")
+	KV_NAMESPACE := os.Getenv("kvNamespace")
+	ctx := context.Background()
+	client, err := cloudflare.New(API_KEY, API_EMAIL, cloudflare.UsingAccount(ACCOUNT_ID))
+	check(err)
+	key := website
+	resp, err := client.ReadWorkersKV(ctx, KV_NAMESPACE, key)
+	check(err)
+	password, err := base64.StdEncoding.DecodeString(string(resp))
+	check(err)
+	fmt.Println("your password is", string(password))
+}
+
+func deletePassword(website string) {
+	API_KEY := os.Getenv("apiKey")
+	API_EMAIL := os.Getenv("apiMail")
+	ACCOUNT_ID := os.Getenv("accountID")
+	KV_NAMESPACE := os.Getenv("kvNamespace")
+	ctx := context.Background()
+	client, err := cloudflare.New(API_KEY, API_EMAIL, cloudflare.UsingAccount(ACCOUNT_ID))
+	check(err)
+	key := website
+	resp, err := client.DeleteWorkersKV(ctx, KV_NAMESPACE, key)
 	check(err)
 	fmt.Println(resp)
 }
 
 func main() {
-	var userInput string
-	var website string
-	fmt.Println("website you use with the password: ")
-	fmt.Scan(&website)
-	fmt.Println("enter password: ")
-	fmt.Scan(&userInput)
-	encodePassword(website, userInput)
+	err := godotenv.Load(".env")
+	check(err)
+	fmt.Println("Welcome to went. Press 1 for saving a password, 2 for retrieving a password and 3 for deleting a password")
+	var option int
+	fmt.Scanln(&option)
+	if option == 1 {
+		var userInput string
+		var website string
+		fmt.Println("website you use with the password: ")
+		fmt.Scan(&website)
+		fmt.Println("enter password: ")
+		fmt.Scan(&userInput)
+		encodePassword(website, userInput)
+	} else if option == 2 {
+		var website string
+		fmt.Println("website you use the password with: ")
+		fmt.Scan(&website)
+		getPassword(website)
+	} else if option == 3 {
+		var website string
+		fmt.Println("website who's password you want to delete: ")
+		fmt.Scan(&website)
+		deletePassword(website)
+	}
 }
